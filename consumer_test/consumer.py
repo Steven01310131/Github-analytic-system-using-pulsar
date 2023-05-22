4c6b777deaf3ffa3dfe933e0f34debdd1254a1f5
Maxlytrius <Jarziz80@gmail.com>
2023-05-22 15:51:25 +0000

    Barebones producer/consumer for testing

--- consumer_test/consumer.py
+++ consumer_test/consumer.py
@@ -5,16 +5,11 @@ from pprint import pprint
 client = pulsar.Client('pulsar://192.168.2.43:6650')
 data={}
 # Subscribe to a topic and subscription
-consumer1 = client.subscribe('Repos2', subscription_name='DE-sub')
-consumer2= client.subscribe('Commits2', subscription_name='DE-sub')
+consumer = client.subscribe('Repos2', subscription_name='DE-sub')
+#consumer2= client.subscribe('Commits2', subscription_name='DE-sub')
 while True:
-    msg1= consumer1.receive()
-    repository=json.loads(msg1.data().decode('utf-8'))
-    repo_name=repository['full_name']
-    language=repository['language']
-    msg2 = consumer2.receive()
-    commits =json.loads(msg2.data().decode('utf-8'))
-    num_commits=int(commits['Link'].split('&page=')[-1].split(';')[0].split('>')[0])
-    data[f'{repo_name}']=[language,num_commits]
-    print(data)
+    msg = consumer.receive(timeout_millis=20000)
+    test =  msg.data().decode('utf-8')
+    consumer.acknowledge(msg)
+    print(test)
 client.close()
