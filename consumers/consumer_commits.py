ec94ce70816e07a459cb6aef82bd551f6a8d701d
Maxlytrius <44470484+Maxlytrius@users.noreply.github.com>
2023-05-24 11:51:22 +0200

    First try at consumer

--- consumers/consumer_commits.py
+++ consumers/consumer_commits.py
@@ -13,20 +13,37 @@ client = pulsar.Client('pulsar://192.168.2.51:6650')
 data={}
 # Subscribe to a topic and subscription
 consumer1 = client.subscribe('repositories_testtest3', subscription_name='question2')
+i_backwards = 49
+num_commits = 0
 while True:
     msg1= consumer1.receive()
     repo_name=msg1.data().decode('utf-8')
     # commits=requests.get(f"https://api.github.com/repos/{repo_name}/commits?per_page=1&page=1") # without token 
-    commits=requests.get(f"https://api.github.com/repos/{repo_name}/commits?per_page=1&page=1",headers=headers)
-    commits = commits.headers
-    num_commits=int(commits['Link'].split('&page=')[-1].split(';')[0].split('>')[0])
+    r = requests.get(f"https://github.com/{repo_name}")
+    commits_index = r.text.find("Commits")
+    i = commits_index - i_backwards
+    commits_str = ""
+    commits_magnitude = 0
+    c = r.text[i]
+    # Unlikely that nr of commits reach 10^6
+    while c != '>' and commits_magnitude < 6:
+        commits_str = c + commits_str
+        i -= 1
+        c = r.text[i]
+        commits_magnitude += 1
+    try:
+        num_commits = int(commits_str)
+    except:
+        print(f"failure for {repo_name}") #TODO remove after testing
+        continue
+
     filter = {
         "full_name": repo_name
     }
 
     update = {
         "$set": {
-            "commits": num_commits
+            "commits": commits_str
         }
     }
 
